2c59867217cba9e6156a02ea2df8c4fb2316040f
Yufeng Huang <yufeng.chase.huang@gmail.com>
2019-02-20 10:25:05 -0800

    crated cc class

--- chemcluster/__init__.py
+++ chemcluster/__init__.py
@@ -2,4 +2,4 @@ r'''
 ChemCluster is a data management system to perform extraction, transformation and loading (ETL) of surface sites from nanoparticles to nanoclusters.
 '''
 
-__all__ = []
+from .cc import CC
---
+++ chemcluster/cc.py
@@ -0,0 +1,53 @@
+from .np import *
+
+class CC:
+    '''
+        This is the main object to perform tasks on ChemCluster
+    '''
+
+    def __init__(self):
+
+        # first establishes SQL connections
+
+        pass
+
+    def add_np(self, xyzFile, description, remove=None):
+        pass
+
+    def show_np(self):
+        # show the saved nanoparticles in the database
+        pass
+
+    def print_np(self, index):
+        pass
+
+    def add_incar(self):
+        # add an INCAR file to the database
+        pass
+
+    def show_incar(self):
+        pass
+
+    def print_incar(self, index):
+        pass
+
+    def add_kpoints(self):
+        pass
+
+    def show_kpoints(self):
+        pass
+
+    def print_kpoints(self, index):
+        pass
+
+    def show_databases(self):
+        pass
+
+    def add_qm(self, database):
+        pass
+
+    def show_qm(self, database):
+        pass
+
+    def print_qm(self, database, index):
+        pass
